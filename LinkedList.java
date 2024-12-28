edd796d132c96d9d20ab63704cdadad8cf42db33
BhagwanSingh Rana <ranabhagwan8815@gmail.com>
2024-12-28 22:07:23 +0530

    linkedlist insertion and deletion code

--- LinkedList.java
+++ LinkedList.java
@@ -95,12 +95,87 @@ public class LinkedList {
         System.out.println("null");
     }
 
+
+    //5. delete node from first
+    public int DeleteFirst(){
+      // if linkedlist is empty
+       if(head==null){
+        System.out.println("ll is empty");
+       }
+       if(size==1){
+        int val =head.data;
+        head=tail=null;
+        size=0;
+        return val;
+       }
+       int val = head.data;
+       head = head.next;
+       size--;
+       return val;
+    }
+
+    //6. delete from last
+    public int deleteLast(){
+         // if linkedlist is empty
+       if(head==null){
+        System.out.println("ll is empty");
+       }
+       if(size==1){
+        int val =head.data;
+        head=tail=null;
+        size=0;
+        return val;
+       }
+       int i=0;
+       Node prev= head;
+       while(i<size-2){
+        prev = prev.next;
+        i++;
+       }
+       int val =tail.data;
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+      
+    }
+
+    //7. delete from middle or a particular index 
+    public int deleteMiddle(int index){
+          // if linkedlist is empty
+       if(head==null){
+        System.out.println("ll is empty");
+        return -1;
+       }
+      if(size<=index){
+        System.out.println("cannot delete a node because size is less or cannot find index");
+        return -1;
+      }
+      if(size==1){
+        int val=head.data;
+        head=tail=null;
+        size=0;
+        return val;
+      }
+       Node prev =head;
+       int i=0;
+        while(i<index-1){
+            prev = prev.next;
+            i++;
+        }
+        Node temp = prev.next;
+        int val = temp.data;
+        prev.next = temp.next;
+        size--;
+        return val;
+    }
+
     // Main method to test the linked list
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
 
         // Testing the linked list
-        ll.addFirst(1); // 2 -> 1 -> 5 -> 3 -> 4 -> null
+        ll.addFirst(1); // 2 -> 1 -> 5 -> 3 -> 4 ->6 ->7 -> null
         ll.addFirst(2);
         ll.addLast(3);
         ll.addLast(4);
@@ -108,7 +183,17 @@ public class LinkedList {
         ll.addLast(7);
         ll.addMiddle(5, 2);
 
-        ll.printLinkedList(); // Expected Output: 2 -> 1 -> 5 -> 3 -> 4 -> null
+        ll.printLinkedList();
+        System.out.println("first node is deleted :"+ll.DeleteFirst()); //1,5,3,4,6,7
+        ll.printLinkedList();
+        System.out.println("last node is deleted :"+ ll.deleteLast());//1,5,3,4,6
+        ll.printLinkedList();
+        System.out.println("delete a particular node "+ll.deleteMiddle(2));  //1,5,4,6
+
+        ll.printLinkedList(); 
+        System.out.println("size of linked list is :"+ ll.size);//4
+
+
     }
 
 }
